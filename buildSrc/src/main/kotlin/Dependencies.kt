4f1c26c679f55e2acdfb7d3d26fabe748eb94ed5
Aleksandr <79718551+zoerAleksandr@users.noreply.github.com>
2026-04-29 13:50:25 +0300

    новая версия 2.2.6.4

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -66,8 +66,8 @@ object Apps {
     const val min_sdk_version = 26
     const val target_sdk_version = 35
 
-    const val version_code = 75
-    const val version_name = "2.2.6.3"
+    const val version_code = 76
+    const val version_name = "2.2.6.4"
 
     const val jvm_target_version = "21"
     val java_compatibility_version = JavaVersion.VERSION_21
