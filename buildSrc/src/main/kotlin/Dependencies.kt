c34b23643741b44002259afa1318696f22fff3ad
Aleksandr <79718551+zoerAleksandr@users.noreply.github.com>
2026-08-22 21:33:20 +0300

    поднял version_code до 79; version_name  3.0.2

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -64,8 +64,8 @@ object Apps {
     const val min_sdk_version = 26
     const val target_sdk_version = 35
 
-    const val version_code = 78
-    const val version_name = "3.0.1"
+    const val version_code = 79
+    const val version_name = "3.0.2"
 
     const val jvm_target_version = "21"
     val java_compatibility_version = JavaVersion.VERSION_21
